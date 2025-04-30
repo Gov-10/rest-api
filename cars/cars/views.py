b7fc71129331f50e4efbabd3fd53d71faaed7178
Govind <govindsys1008@gmail.com>
2025-04-30 22:08:10 +0530

    implemented GET method

--- cars/cars/views.py
+++ cars/cars/views.py
@@ -1,8 +1,14 @@
 from django.http import JsonResponse
 from .models import Car
 from .serializers import CarModel
+from rest_framework.decorators import api_view
 
-def get_cars(request):
-    cars = Car.objects.all()
-    serializer = CarModel(cars, many=True)
-    return JsonResponse(serializer.data, safe = False)
+@api_view(['GET', 'POST']) #here ap_view is a decorator
+def get_cars(request): 
+    if request.method == 'GET':
+       cars = Car.objects.all()
+       serializer = CarModel(cars, many=True)
+       return JsonResponse(serializer.data, safe = False)
+    if request.method == 'POST':
+        serializer  = CarModel(data = request.data)
+        
